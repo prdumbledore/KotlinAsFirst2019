eaa9f7098e6d572da05041b2624123c834831500
sargsyan.ee <erik.sargsyan@mail.ru>
2019-11-04 23:28:52 +0300

    6 lesson 1 part

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -114,7 +114,7 @@ class Tests {
     @Test
     @Tag("Hard")
     fun mostExpensive() {
-        assertEquals("", mostExpensive(""))
+        assertEquals("Any good with price 0.0", mostExpensive("а 0"))
         assertEquals("Курица", mostExpensive("Хлеб 39.9; Молоко 62.5; Курица 184.0; Конфеты 89.9"))
         assertEquals("Вино", mostExpensive("Вино 255.0"))
     }
